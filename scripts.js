0fe3d0e6290579926cf4e4a6f89154d284a9f9fe
joaocorreiaprof <joaocorreiaprof@gmail.com>
2024-01-30 19:50:47 +0000

    Logical expressions and math functions

---
+++ scripts.js
@@ -0,0 +1,65 @@
+//1: Math Functions
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// 2: Declare variables
+let displayValue = "";
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+//3: Operate Function
+
+function operate(firstNumber, operator, secondNumber) {
+  let finalResult;
+
+  if (operator === "+") {
+    finalResult = add(firstNumber, secondNumber);
+    console.log(finalResult);
+  } else if (operator === "-") {
+    finalResult = subtract(firstNumber, secondNumber);
+  } else if (operator === "X") {
+    finalResult = multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    finalResult = divide(firstNumber, secondNumber);
+  } else {
+    return "Invalid operator";
+  }
+
+  return finalResult;
+}
+
+// Function to add the pressed number or operator to the display
+function updateDisplay(number) {
+  document.getElementById("display").innerText += number;
+}
+
+// Function to calculate the result
+function calculate() {
+  let expression = document.getElementById("display").innerText;
+
+  for (let index = 0; index < expression.length; index++) {
+    if (expression[index] === "+") {
+      let operator = "+";
+      let firstNumber = parseInt(expression.slice(0, index));
+      let secondNumber = parseInt(expression.slice(index + 1));
+      console.log(firstNumber);
+      console.log(secondNumber);
+      let result = operate(firstNumber, operator, secondNumber);
+      return result;
+    }
+  }
+}
