e15d3a166f2e919994a05fa342921d6d6f457d7b
joaocorreiaprof <joaocorreiaprof@gmail.com>
2024-02-02 21:25:47 +0000

    Added the option to calculate more than 2 numbers

--- scripts.js
+++ scripts.js
@@ -46,44 +46,25 @@ function operate(firstNumber, operator, secondNumber) {
 function updateDisplay(number) {
   document.getElementById("display").innerText += number;
 }
-
 // Function to calculate the result
 function calculate() {
   let expression = document.getElementById("display").innerText;
 
-  for (let index = 0; index < expression.length; index++) {
-    if (expression[index] === "+") {
-      operator = "+";
-      firstNumber = parseInt(expression.slice(0, index));
-      secondNumber = parseInt(expression.slice(index + 1));
-      finalResult = operate(firstNumber, operator, secondNumber);
-      document.getElementById("display").innerText = finalResult;
-      return finalResult;
-    }
-    if (expression[index] === "-") {
-      operator = "-";
-      firstNumber = parseInt(expression.slice(0, index));
-      secondNumber = parseInt(expression.slice(index + 1));
-      finalResult = operate(firstNumber, operator, secondNumber);
-      document.getElementById("display").innerText = finalResult;
-      return finalResult;
-    }
-    if (expression[index] === "X") {
-      operator = "X";
-      firstNumber = parseInt(expression.slice(0, index));
-      secondNumber = parseInt(expression.slice(index + 1));
-      finalResult = operate(firstNumber, operator, secondNumber);
-      document.getElementById("display").innerText = finalResult;
-      return finalResult;
-    }
-    if (expression[index] === "/") {
-      operator = "/";
-      firstNumber = parseInt(expression.slice(0, index));
-      secondNumber = parseInt(expression.slice(index + 1));
-      finalResult = operate(firstNumber, operator, secondNumber);
-      document.getElementById("display").innerText = finalResult;
-      return finalResult;
+  let operators = ["+", "-", "X", "/"];
+  let numbers = expression.split(/[-+X/]/).map(parseFloat);
+  let operations = expression.split(/[0-9.]+/).filter(Boolean);
+
+  for (let i = 0; i < operators.length; i++) {
+    while (operations.includes(operators[i])) {
+      let index = operations.indexOf(operators[i]);
+      let result = operate(numbers[index], operators[i], numbers[index + 1]);
+
+      numbers.splice(index, 2, result);
+      operations.splice(index, 1);
+
+      document.getElementById("display").innerText = result;
     }
   }
-  console.log(expression);
+
+  return parseFloat(document.getElementById("display").innerText);
 }
